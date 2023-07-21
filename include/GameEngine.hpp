d8cd97ab9bd6af981a39143593f4ca89c0f7a543
AnnaOlla <140060248+AnnaOlla@users.noreply.github.com>
2023-07-21 14:38:09 +0400

    Add files via upload

--- include/GameEngine.hpp
+++ include/GameEngine.hpp
@@ -5,6 +5,7 @@
 #include "RandomNumberGenerator.hpp"
 
 #include <vector>
+#include <algorithm>
 
 class GameEngine
 {
@@ -15,10 +16,8 @@ class GameEngine
         bool selectTile(const int, const int);
         bool deselectTile();
         bool moveToTile(const int, const int);
-
         void addExpectedBalls(const int);
         void transformExpectedBalls();
-
 
     private:
         std::vector <std::vector <Tile>> m_tileMap;
@@ -29,9 +28,28 @@ class GameEngine
         int m_score;
 
         int m_colorCount;
-        int m_combinationLength;
+        int m_minStreakLength;
 
         RandomNumberGenerator random;
+
+        bool isTilePassable(const int, const int) const;
+        bool findPath(const int, const int, const int, const int, std::vector <std::pair <int, int>>&) const;
+
+        void deleteStreaks(const int, const int);
+
+        bool isHorizontalStreak(const int, const int) const;
+        int deleteAdjacentHorizontalStreak(const int, const int);
+
+        bool isVerticalStreak(const int, const int) const;
+        int deleteAdjacentVerticalStreak(const int, const int);
+
+        bool isMainDiagonalStreak(const int, const int) const;
+        int deleteAdjacentMainDiagonalStreak(const int, const int);
+
+        bool isAntiDiagonalStreak(const int, const int) const;
+        int deleteAdjacentAntiDiagonalStreak(const int, const int);
+
+        void addScores(const int);
 };
 
 #endif // GAMEENGINE_HPP
